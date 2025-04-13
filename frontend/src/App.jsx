365f7e585ba51b4218fb62f010d176da4835ea90
Ayush <ayush0146patel@gmail.com>
2025-04-13 21:27:50 +0530

    Initial Commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,36 @@
+import './App.css'
+import VideoPlayer from './VideoPlayer'
+import { useRef } from 'react'
+
+function App() {
+  const playerRef = useRef(null)
+  const videoLink = "http://localhost:8000/uploads/courses/a5734385-9122-4f3b-9bec-f65dbcf59c22/index.m3u8";
+
+  const videoPlayerOptions = {
+    controls: true,
+    responsive: true,
+    fluid: true,
+    sources: [
+      {
+        src: videoLink,
+        type: "application/x-mpegURL"
+      }
+    ]
+  }
+  const handlePlayerReady = (player) => {
+    playerRef.current = player;
+  };
+  return (
+    <>
+      <div>
+        <h1>Video player</h1>
+      </div>
+      <VideoPlayer
+      options={videoPlayerOptions}
+      onReady={handlePlayerReady}
+      />
+    </>
+  )
+}
+
+export default App
